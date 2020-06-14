5eac5bb5a56d2192d06d94e44b18f75012a492dc
Roarkz <rory_bell_88@hotmail.com>
2020-06-14 16:03:57 +1000

    a bit of work on the contact form. Basic stlying and content is in.

--- src/popups/contact.js
+++ src/popups/contact.js
@@ -3,37 +3,35 @@ import React, {Component} from 'react'
 const ContactPage = () => {
   return (
     <>
+      <div id="inner-content">
       <h2>Contact</h2>
-      <p>Thankyou for your interest in allowing me to undertake a project for you. Please fill in the below details
-       and ill be in touch via your prefered contact method as soon as i can.
-
-       Kindest regards,
-
-       Rory Bell
-      </p>
-      <div>
-        <form>
-          <label>
-            Name: 
+        <p>Thankyou for your interest in allowing me to undertake a project for you. Please fill in the below details
+         and ill be in touch via your prefered contact method as soon as i can.
+        </p>
+        <p>Kindest regards, Rory Bell.</p>
+        <div id="form-wrapper">
+          <form>
+            <label>Name</label>
             <input type="text" name="name" />
-          </label><br></br>
-          <label>
-            Email: 
+
+            <label>Email</label>
             <input type="email" name="email" />
-          </label><br></br>
-          <label>
-            This section is for you to leave some notes about your project or any basic information you want to fill me in on.
-            We can get into the nitty gritty when we touch base. Any information you provide here will be sent to me via email.<br></br>
+
+            <p>
+              This section is for you to leave some notes about your project or any basic information you want to fill me in on.
+              We can get into the nitty gritty when we touch base. Any information you provide here will be sent to me via email.
+            </p>
             <textarea id="w3review" name="user-notes" rows="4" cols="50"></textarea>
-          </label><br></br>
-          <label>
-            Prefered contact method?
-          </label>
-            <input type="radio" id="email" name="age" value="30" />
-            <label for="email">Email</label>
-            <input type="radio" id="mobile" name="age" value="60" />
-            <label for="mobile">Mobile</label>
-        </form>
+
+            <label>Prefered contact method?</label>
+            <div>
+              <input type="radio" id="email" name="age" value="30" />
+              <label for="email">Email</label>
+              <input type="radio" id="mobile" name="age" value="60" />
+              <label for="mobile">Mobile</label>
+            </div>
+          </form>
+        </div>
       </div>
       </>
   )
